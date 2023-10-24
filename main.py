194bc649a7e345f99bfb6809971be37735b97b51
Eeham Khan <eehamkhan@hotmail.com>
2023-10-24 17:06:36 -0400

    Created initial one hot vectors for the penguins CSV file.

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import pandas as pd
 import numpy as np
-
+from sklearn.preprocessing import OneHotEncoder
 
 data = pd.read_csv("penguins.csv")
+categories = ['island', 'sex']
+
+ohe = OneHotEncoder(sparse_output=False)
+encoded = ohe.fit_transform(data[categories])
+df = pd.DataFrame(encoded, columns=ohe.get_feature_names_out(categories))
+
+data = data.drop(columns=categories)
+
+data = pd.concat([data, df], axis=1)
+
+print(data)
